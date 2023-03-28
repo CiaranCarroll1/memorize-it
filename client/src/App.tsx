f95abd27cbaff1054b6ea0875018aefaed42bf84
Ciaran Carroll <ciaran.carroll1@gmail.com>
2023-03-28 23:57:25 +1100

    Make start on guessing game

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,139 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import { TiTick, TiTimes } from 'react-icons/ti';
 
-function App() {
-  const [count, setCount] = useState(0)
+type Card = {
+  selected: Boolean;
+  color: string;
+};
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+enum GameState {
+  None = 'none',
+  Started = 'started',
+  Guessing = 'guessing',
+  Correct = 'correct',
+  Wrong = 'wrong',
 }
 
-export default App
+const initialCards: string[] = [
+  'red',
+  'blue',
+  'pink',
+  'purple',
+  'yellow',
+  'orange',
+  'brown',
+  'green',
+  'indigo',
+];
+
+function App() {
+  const [cards, setCards] = useState<string[]>(initialCards);
+  const [selected, setSelected] = useState<number>(0);
+  const [gameState, setGameState] = useState<GameState>(GameState.None);
+
+  const shuffle = (): void => {
+    const newCards: string[] = [...cards];
+    let currentIndex = newCards.length,
+      randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [newCards[currentIndex], newCards[randomIndex]] = [
+        newCards[randomIndex],
+        newCards[currentIndex],
+      ];
+    }
+
+    setCards(newCards);
+  };
+
+  const reset = (): void => {
+    shuffle();
+    setSelected(Math.floor(Math.random() * 10));
+  };
+
+  const handlePlayClick = () => {
+    setGameState(GameState.Started);
+    setTimeout(() => {
+      setGameState(GameState.Guessing);
+    }, 3000);
+  };
+
+  const handleAttemptClick = (index: number) => {
+    if (index === selected) {
+      console.log('Correct');
+      setGameState(GameState.Correct);
+    } else {
+      console.log('Incorrect');
+      setGameState(GameState.Wrong);
+    }
+    setTimeout(() => {
+      reset();
+      setGameState(GameState.None);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    reset();
+  }, []);
+
+  return (
+    <div className="w-full h-screen bg-blue-900 text-gray-300">
+      {/* Toolbar */}
+      <div className="absolute w-full h-[50px] flex justify-between items-center px-3">
+        <h1 className="text-2xl">Memorize-It</h1>
+
+        <div className="flex justify-center gap-4 text-lg">
+          <button
+            className="py-1 px-5 border border-gray-300 rounded hover:border-yellow-500 hover:bg-yellow-500"
+            onClick={handlePlayClick}
+          >
+            Play
+          </button>
+          {/* <button className="py-1 px-5 border border-gray-300 rounded hover:border-yellow-500 hover:bg-yellow-500">
+            Scoreboard
+          </button> */}
+        </div>
+      </div>
+
+      {/* Game */}
+      <div className="w-full h-full flex justify-center items-center">
+        <div className="grid grid-cols-3 h-[500px] w-[500px] gap-5">
+          {cards.map((color: string, index: number) => {
+            return (
+              <div
+                key={index}
+                className="border border-gray-300 rounded-lg cursor-pointer"
+                style={{
+                  backgroundColor:
+                    gameState === GameState.Started ||
+                    gameState === GameState.Correct ||
+                    gameState === GameState.Wrong
+                      ? color
+                      : 'slategray',
+                }}
+                onClick={() => handleAttemptClick(index)}
+              ></div>
+            );
+          })}
+        </div>
+        {(gameState === GameState.Guessing ||
+          gameState === GameState.Correct ||
+          gameState === GameState.Wrong) && (
+          <div
+            className="flex justify-center items-center  w-[100px] h-[100px] mx-12 border border-gray-300 rounded-lg"
+            style={{ backgroundColor: cards[selected] }}
+          >
+            {gameState === GameState.Correct && <TiTick color="black" />}
+            {gameState === GameState.Wrong && <TiTimes color="black" />}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
