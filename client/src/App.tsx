aeba91ddb1eec379b4849b58bd0629947c46619f
Ciaran Carroll <ciaran.carroll1@gmail.com>
2023-03-29 12:14:10 +1100

    Tidy up flow and add status message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,7 +12,7 @@ enum GameState {
   Started = 'started',
   Guessing = 'guessing',
   Correct = 'correct',
-  Wrong = 'wrong',
+  Incorrect = 'incorrect',
 }
 
 const initialCards: string[] = [
@@ -29,8 +29,9 @@ const initialCards: string[] = [
 
 function App() {
   const [cards, setCards] = useState<string[]>(initialCards);
-  const [selected, setSelected] = useState<number>(0);
   const [gameState, setGameState] = useState<GameState>(GameState.None);
+  const [correctCard, setCorrectCard] = useState<number>(0);
+  const [guessedCard, setGuessedCard] = useState<number>(0);
 
   const shuffle = (): void => {
     const newCards: string[] = [...cards];
@@ -50,9 +51,24 @@ function App() {
     setCards(newCards);
   };
 
+  const getStatusMessage = (): string => {
+    if (gameState === GameState.Started) {
+      return 'Memorize position of each color!';
+    } else if (gameState === GameState.Guessing) {
+      return 'Click the card that matches the color!';
+    } else if (gameState === GameState.Correct) {
+      return 'Too easy!';
+    } else if (gameState === GameState.Incorrect) {
+      return 'Hard luck!';
+    } else {
+      return 'Click play to start!';
+    }
+  };
+
   const reset = (): void => {
     shuffle();
-    setSelected(Math.floor(Math.random() * 10));
+    setCorrectCard(Math.floor(Math.random() * 10));
+    setGuessedCard(-1);
   };
 
   const handlePlayClick = () => {
@@ -63,17 +79,26 @@ function App() {
   };
 
   const handleAttemptClick = (index: number) => {
-    if (index === selected) {
+    setGuessedCard(index);
+    if (index === correctCard) {
       console.log('Correct');
       setGameState(GameState.Correct);
     } else {
       console.log('Incorrect');
-      setGameState(GameState.Wrong);
+      setGameState(GameState.Incorrect);
     }
     setTimeout(() => {
       reset();
       setGameState(GameState.None);
     }, 3000);
+  };
+
+  const colorToShow = (color: string) => {
+    return gameState === GameState.Started ||
+      gameState === GameState.Correct ||
+      gameState === GameState.Incorrect
+      ? color
+      : 'slategray';
   };
 
   useEffect(() => {
@@ -83,13 +108,14 @@ function App() {
   return (
     <div className="w-full h-screen bg-blue-900 text-gray-300">
       {/* Toolbar */}
-      <div className="absolute w-full h-[50px] flex justify-between items-center px-3">
-        <h1 className="text-2xl">Memorize-It</h1>
+      <div className=" w-full h-[50px] flex justify-between items-center px-3">
+        <h1 className="text-3xl text-yellow-500">Memorize-It</h1>
 
         <div className="flex justify-center gap-4 text-lg">
           <button
-            className="py-1 px-5 border border-gray-300 rounded hover:border-yellow-500 hover:bg-yellow-500"
+            className="py-1 px-5 border border-gray-300 rounded hover:border-yellow-500 hover:bg-yellow-500 disabled:border-gray-700 disabled:bg-gray-700 disabled:hover:border-gray-700 disabled:hover:bg-gray-700"
             onClick={handlePlayClick}
+            disabled={gameState !== GameState.None}
           >
             Play
           </button>
@@ -99,37 +125,46 @@ function App() {
         </div>
       </div>
 
+      {/* Status */}
+      <div className="w-full flex justify-center text-2xl bold">
+        {getStatusMessage()}
+      </div>
+
       {/* Game */}
-      <div className="w-full h-full flex justify-center items-center">
+      <div className="w-full h-[600px] flex justify-center items-center">
         <div className="grid grid-cols-3 h-[500px] w-[500px] gap-5">
           {cards.map((color: string, index: number) => {
             return (
               <div
                 key={index}
-                className="border border-gray-300 rounded-lg cursor-pointer"
+                className="flex justify-center items-center border border-gray-300 rounded-lg cursor-pointer"
                 style={{
-                  backgroundColor:
-                    gameState === GameState.Started ||
-                    gameState === GameState.Correct ||
-                    gameState === GameState.Wrong
-                      ? color
-                      : 'slategray',
+                  backgroundColor: colorToShow(color),
                 }}
                 onClick={() => handleAttemptClick(index)}
-              ></div>
+              >
+                {index === guessedCard ? (
+                  <>
+                    {gameState === GameState.Correct ? (
+                      <TiTick color="black" size={150} />
+                    ) : (
+                      <TiTimes color="black" size={150} />
+                    )}
+                  </>
+                ) : (
+                  <TiTimes color={colorToShow(color)} size={150} />
+                )}
+              </div>
             );
           })}
         </div>
         {(gameState === GameState.Guessing ||
           gameState === GameState.Correct ||
-          gameState === GameState.Wrong) && (
+          gameState === GameState.Incorrect) && (
           <div
-            className="flex justify-center items-center  w-[100px] h-[100px] mx-12 border border-gray-300 rounded-lg"
-            style={{ backgroundColor: cards[selected] }}
-          >
-            {gameState === GameState.Correct && <TiTick color="black" />}
-            {gameState === GameState.Wrong && <TiTimes color="black" />}
-          </div>
+            className="  w-[100px] h-[100px] mx-12 border border-gray-300 rounded-lg"
+            style={{ backgroundColor: cards[correctCard] }}
+          ></div>
         )}
       </div>
     </div>
